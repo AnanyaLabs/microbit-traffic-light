77b30755dc519d1fdad2ef8354a958df1e0b5e8a
Ananya <thakurananyasingh2002@gmail.com>
2026-06-08 11:49:00 +0530

    Create traffic_light.js

---
+++ traffic_light.js
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    basic.showString("R")
+    basic.pause(3000)
+
+    basic.showString("Y")
+    basic.pause(1000)
+
+    basic.showString("G")
+    basic.pause(3000)
+})
